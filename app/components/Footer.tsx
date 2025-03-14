ffba21c22e4e6880cf34e758c75b23efd3ea5d08
Jason Lee Wilson <robotici@yahoo.com>
2025-03-14 00:53:42 -0500

    updated footer copy

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,14 +1,21 @@
 import { Link } from "@remix-run/react";
 import Social from "./Social";
-// import { FaHeart } from "react-icons/fa6";
+import { FaHeart } from "react-icons/fa6";
 
 function Footer() {
   return (
     <>
       <footer className='p-4'>
         <Social />
+        <p className='text-center text-white mb-4 h-6'>
+          &copy; 2024-2025{" "}
+          <Link to='https://pressonthese.com/' target='_blank' rel='noreferrer'>
+            pressonthese.com
+          </Link>
+          .
+        </p>
         <p className='text-center text-white h-6'>
-          Made with ♥️ by:{" "}
+          Made with <FaHeart className='inline-block' /> by:{" "}
           <Link
             to='https://jasonleewilson.com/'
             target='_blank'
