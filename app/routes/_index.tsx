03a90332d2f8433c902d3dfd74803b2cdc103efc
Jason Lee Wilson <robotici@yahoo.com>
2025-03-21 11:08:46 -0500

    update share link copy

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,11 +8,11 @@ export const meta: MetaFunction = () => {
     { title: "Press On These" },
     {
       name: "description",
-      content: "PRESSONTHESE.COM - HANDMADE. CUSTOM. DURABLE. reusable.",
+      content: "PRESSONTHESE.COM - HANDMADE. CUSTOM. DURABLE. .REUSABLE.",
     },
     {
       property: "og:title",
-      content: "PRESSONTHESE.COM - HANDMADE. CUSTOM. DURABLE. reusable.",
+      content: "PRESSONTHESE.COM - HANDMADE. CUSTOM. DURABLE. .REUSABLE.",
     },
     {
       property: "og:description",
